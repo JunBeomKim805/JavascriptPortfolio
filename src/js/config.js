ded17edd889084f9914721832b5bf4f6682c3d5f
junbeomKim <kimjunbum00805@gmail.com>
2021-09-12 10:56:49 -0400

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '4b1af25f-e1e8-480b-b60c-cb1419a7f580';
+export const MODAL_CLOSE_SEC = 2.5;
